87d37de5cbcb22bd1c1f5131b76d16f15d038498
Bhagirathsinh Rathod <wizard.tech.54@gmail.com>
2023-04-21 22:30:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,119 @@
+import numpy as np
+import pickle
+import pandas as pd
+import streamlit as st 
+from PIL import Image
+import nltk
+
+## utilities
+import re
+import pickle
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+## nltk
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+pickleModel = open("Sentiment-LR.pickle","rb")
+classifier = pickle.load(pickleModel)
+
+picklevectorizer = open("vectoriser-ngram-(1,2).pickle","rb")
+vectorizer = pickle.load(picklevectorizer)
+
+## Defining all the emojis with their meanings.
+emojis = {':)': 'smile', ':-)': 'smile', ';d': 'wink', ':-E': 'vampire', ':(': 'sad', 
+          ':-(': 'sad', ':-<': 'sad', ':P': 'raspberry', ':O': 'surprised',
+          ':-@': 'shocked', ':@': 'shocked',':-$': 'confused', ':\\': 'annoyed', 
+          ':#': 'mute', ':X': 'mute', ':^)': 'smile', ':-&': 'confused', '$_$': 'greedy',
+          '@@': 'eyeroll', ':-!': 'confused', ':-D': 'smile', ':-0': 'yell', 'O.o': 'confused',
+          '<(-_-)>': 'robot', 'd[-_-]b': 'dj', ":'-)": 'sadsmile', ';)': 'wink', 
+          ';-)': 'wink', 'O:-)': 'angel','O*-)': 'angel','(:-D': 'gossip', '=^.^=': 'cat'}
+
+## Defining set containing all stopwords in english.
+stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an',
+             'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
+             'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
+             'does', 'doing', 'down', 'during', 'each','few', 'for', 'from', 
+             'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
+             'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+             'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
+             'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
+             'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're',
+             's', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
+             't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
+             'themselves', 'then', 'there', 'these', 'they', 'this', 'those', 
+             'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
+             'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
+             'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
+             "youve", 'your', 'yours', 'yourself', 'yourselves']
+
+def welcome():
+    return "Welcome All"
+
+def preprocess_text(tweet):
+    
+    # Create Lemmatizer and Stemmer.
+    wordLemm = WordNetLemmatizer()
+    
+    # Defining regex patterns.
+    urlPattern        = r"((http://)[^ ]*|(https://)[^ ]*|( www\.)[^ ]*)"
+    userPattern       = '@[^\s]+'
+    alphaPattern      = "[^a-zA-Z0-9]"
+    sequencePattern   = r"(.)\1\1+"
+    seqReplacePattern = r"\1\1"
+    
+    
+    tweet = tweet.lower()
+    
+    # Replace all URls with 'URL'
+    tweet = re.sub(urlPattern,' URL',tweet)
+    # Replace all emojis.
+    for emoji in emojis.keys():
+        tweet = tweet.replace(emoji, "EMOJI" + emojis[emoji])        
+    # Replace @USERNAME to 'USER'.
+    tweet = re.sub(userPattern,' USER', tweet)        
+    # Replace all non alphabets.
+    tweet = re.sub(alphaPattern, " ", tweet)
+    # Replace 3 or more consecutive letters by 2 letter.
+    tweet = re.sub(sequencePattern, seqReplacePattern, tweet)
+
+    tweetwords = ''
+    for word in tweet.split():
+        # Checking if the word is a stopword.
+        #if word not in stopwordlist:
+        if len(word)>1:
+            # Lemmatizing the word.
+            word = wordLemm.lemmatize(word)
+            tweetwords += (word+' ')
+        
+    return tweet
+
+def predict(newTweet):
+    newTweet = preprocess_text(newTweet)
+    newTweet_vectorized = vectorizer.transform([newTweet])
+    prediction=classifier.predict(newTweet_vectorized)
+    print(prediction)
+    print(prediction[0])
+    return prediction[0]
+
+def main():
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;"> Twitter Sentiment Analysis</h2>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    newTweet = st.text_input("Tweet","Type Here")
+    result=""
+    if st.button("Predict"):
+        scale=predict(newTweet)
+        if scale==1.0:
+            result="Positive"
+        elif scale==0.0:
+            result="Negative"
+    st.success('The output is {}'.format(result))
+
+if __name__=='__main__':
+    main()
